ce753864f17bbbd89aafbe24a1b8fcd5ddfae666
Ильнар Шангареев <i.shangareyev@modulbank.ru>
2022-05-30 17:22:42 +0400

    Компонент UserList, IUser, IAddress

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
 
 const App = () => {
+  const users: IUser[] = [
+    {id:1, name: 'Noor', email: 'asdas@asdas.rr', address: { city: 'Moscow', street: 'Auchan', zipcode: 'sad'}}
+  ]
+
   return (
     <div>
       <Card
@@ -9,6 +15,7 @@ const App = () => {
         width={'300px'}>
         <button>Кнопка</button>
       </Card>
+      <UserList users={users}></UserList>
     </div>
   );
 };
